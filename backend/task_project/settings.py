fc39f8e8785d4b50f58dc3624e95337f00b2b545
Creed <redota.jadecarlo.dll@gmail.com>
2026-05-22 16:35:39 +0800

    fix: set DEBUG=False for production

--- backend/task_project/settings.py
+++ backend/task_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-79tb=lb&fqd4yre#4yn$875y#j@5od%ziqxtd(^@y!&mj3%m00'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
